eff98206f406a9d99a93aba395832149bea2b65d
kezdetphia <fehermark88@gmail.com>
2023-06-13 13:09:32 +0200

    installed express and ejs, setup the server.js and a players database,rendered homepage

---
+++ data/players-db.js
@@ -0,0 +1,15 @@
+const players = [
+  'Roberto Carlos',
+  'Ronaldinho',
+  'R9',
+  'Luis Figo',
+  'Edgar Davids',
+  'David Beckham'
+
+]
+
+module.exports = {
+  getAll: ()=>{
+    return players;
+  }
+}
