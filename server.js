8a59b8dfa8105a32c0e05a6cdf0290ac9cdd1c32
kezdetphia <fehermark88@gmail.com>
2023-06-13 13:27:24 +0200

    star footballers been rendered to the /players route

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+const playersDB = require('./data/players-db')
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
@@ -10,6 +12,9 @@ app.get('/', (req,res)=>{
   res.render('home')
 })
 
-
+app.get('/players', (req,res)=>{
+  const players = playersDB.getAll()
+  res.render('players-fold/players', {players})
+})
 
 app.listen(3000)
